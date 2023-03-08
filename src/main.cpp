9820a639510af2565abe4a90f1ec01287053932f
aed6-ua <aed6@alu.ua.es>
2023-03-08 23:56:38 +0100

    pasado corpus

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,23 @@
 #include <iostream> 
 #include <string>
-#include <list> 
+#include <list>
+#include <sys/resource.h>
 #include "tokenizador.h"
-
 using namespace std;
-
-///////// Comprobación de que vacíe la lista resultado
-
-void imprimirListaSTL(const list<string>& cadena)
-{
-        list<string>::const_iterator itCadena;
-        for(itCadena=cadena.begin();itCadena!=cadena.end();itCadena++)
-        {
-                cout << (*itCadena) << ", ";
-        }
-        cout << endl;
+double getcputime(void) { 
+        struct timeval tim; 
+        struct rusage ru; 
+        getrusage(RUSAGE_SELF, &ru); 
+        tim=ru.ru_utime; 
+        double t=(double)tim.tv_sec + (double)tim.tv_usec / 1000000.0; 
+        tim=ru.ru_stime; 
+        t+=(double)tim.tv_sec + (double)tim.tv_usec / 1000000.0; 
+        return t; 
 }
-
-int
-main(void)
-{
-	bool kCasosEspeciales = true, kpasarAminusculas = false;
-
-	list<string> lt1, lt2;
-
-Tokenizador a("", true, false); 
-list<string> tokens; 
-
-a.Tokenizar("http:", tokens);
-	imprimirListaSTL(tokens);
-
-a.Tokenizar("http:////ab/", tokens);
-	imprimirListaSTL(tokens);
-
-a.Tokenizar("http:////ab.", tokens);
-	imprimirListaSTL(tokens);
-
-
+int main() {
+        long double aa=getcputime();
+        Tokenizador a("\t ,;:.-+/*_`'{}[]()!?&#\"\\<>", true, true);
+        a.TokenizarListaFicheros("listaFicheros.txt"); // TODO EL CORPUS
+        cout << "Ha tardado " << getcputime() - aa << " segundos" << endl;
+        return 0;
 }
